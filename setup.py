bb0c2d45bcdcb0a60d7c50e04eecc343435214e5
Al Zadid Sultan Bin Habib <zadidhabib@Zadids-MacBook-Pro.local>
2025-05-26 01:28:04 -0400

    Bump version to 0.1.4 with updated README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="TabSeq",
-    version="0.1.3",
+    version="0.1.4",
     author="Zadid Habib",
     author_email="ah00069@mix.wvu.edu",
     description="TabSeq: A Framework for Deep Learning on Tabular Data via Sequential Ordering",
--- tabseq/__init__.py
+++ tabseq/__init__.py
@@ -3,7 +3,7 @@ TabSeq: A Framework for Deep Learning on Tabular Data via Sequential Ordering
 """
 
 # Version of the TabSeq package (bump this for each release)
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 # Expose the main training functions at package level
 from .binary import train_binary_model
