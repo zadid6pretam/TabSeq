ad3ffbf599271ec3a7b85e433c71cfb77f15833f
Al Zadid Sultan Bin Habib <zadidhabib@Zadids-MacBook-Pro.local>
2025-05-27 03:57:57 -0400

    Bump tabseq-feature-ordering to version 0.1.2

--- setup_fo.py
+++ setup_fo.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="feature-ordering-tabseq",
-    version="0.1.6",
+    name="tabseq-feature-ordering",
+    version="0.1.2",
     author="Zadid Habib",
     author_email="ah00069@mix.wvu.edu",
     description="Feature Ordering Module from TabSeq (ICPR 2024)",
